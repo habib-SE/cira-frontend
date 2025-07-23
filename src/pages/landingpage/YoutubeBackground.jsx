90ac2fe533256b95c2db4c573bf344f07849a9a7
Haseeb <haseeb262721@gmail.com>
2025-07-23 12:48:28 +0500

    changes

--- src/pages/landingpage/YoutubeBackground.jsx
+++ src/pages/landingpage/YoutubeBackground.jsx
@@ -2,21 +2,28 @@ import React from "react";
 
 const YoutubeBackground = () => {
   return (
-    <div className="relative bg-white h-screen mt-16 mx-3 overflow-hidden">
-      {/* YouTube iframe */}
-      <iframe
+    <div className="relative h-screen mt-16 mx-3 overflow-hidden">
+    
+      <div>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
       className="absolute inset-0 w-full h-full object-cover rounded-3xl"
-        src="https://www.youtube.com/embed/NZ1EBaqDL0M?autoplay=1&mute=1&loop=1&playlist=NZ1EBaqDL0M&controls=0&showinfo=0&modestbranding=1&rel=0"
+        src="https://framerusercontent.com/assets/fPtrUscopc4S3tX6elWB2S5HyQ.mp4"
         title="YouTube video background"
         frameBorder="0"
         allow="autoplay; fullscreen"
         allowFullScreen
-      ></iframe>
+      />
+     
 
-      {/* Dark overlay */}
-      {/* <div className="absolute inset-0  bg-opacity-50"></div> */}
+     
+      <div className="absolute rounded-3xl inset-0 bg-[rgba(15,15,15,0.7)] opacity-70 pointer-events-none" />
+       </div>
 
-      {/* Centered Content */}
+    
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4">
         <h2 className="text-4xl md:text-5xl max-w-5xl font-bold mb-6">Proactive New⎯Gen AI designed to 
 Support Personal Growth</h2>
