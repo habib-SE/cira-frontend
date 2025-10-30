2d954bd40a1458e916c279378e49a5055bfaad21
habib-SE <hab67603@gmail.com>
2025-10-30 11:53:48 +0500

    update icon

--- src/Admin panel/doctor/doctorpages/DoctorDashboard.jsx
+++ src/Admin panel/doctor/doctorpages/DoctorDashboard.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { 
     Clock, 
-    Clock as ClockIcon,
     Users, 
     DollarSign, 
     TrendingUp,
